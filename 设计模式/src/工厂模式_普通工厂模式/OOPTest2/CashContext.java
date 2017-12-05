bbf1c0affbcf136c8086a9a3d2b26b420714369c
SuperSupeng <supeng842499467@gmail.com>
2017-12-05 13:20:46 +0800

    OOP
    策略模式与简单工厂模式的综合应用

--- 设计模式/src/工厂模式_普通工厂模式/OOPTest2/CashContext.java
+++ 设计模式/src/工厂模式_普通工厂模式/OOPTest2/CashContext.java
@@ -10,6 +10,25 @@ public class CashContext {
         this.cashSuper = cashSuper;
     }
 
+    /*
+    策略模式与简单工厂模式的综合应用
+    public static CashSuper CashContext(String type){
+        CashSuper cashSuper = null;
+        switch (type){
+            case "正常收费":
+                cashSuper = new CashNormal();
+                break;
+            case "满300返100":
+                cashSuper = new CashReturn(300,100);
+                break;
+            case "打八折":
+                cashSuper = new CashRebate(0.8);
+                break;
+        }
+        return cashSuper;
+    }
+ */
+
     public double getResult(double money){
         return cashSuper.acceptCash(money);
     }
