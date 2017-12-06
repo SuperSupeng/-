a2d401c145927373c0d8102adc0a82451cf33ec8
SuperSupeng <supeng842499467@gmail.com>
2017-12-06 15:06:10 +0800

    装饰者
    装饰者模式

---
+++ 设计模式/src/装饰者模式/Person.java
@@ -0,0 +1,20 @@
+package 装饰者模式;
+
+/**
+ * Created by ${super} on ${2017/5/27}.
+ */
+public class Person {
+    private String name;
+
+    public Person(){
+
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public void show(){
+        System.out.printf("装扮的%s\n", name);
+    }
+}
