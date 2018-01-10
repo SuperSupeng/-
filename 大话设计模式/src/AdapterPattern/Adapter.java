9143817ecbb3f27226920676a1ba439afa7c1fc4
super <supeng842499467@gmail.com>
2018-01-10 08:21:46 +0800

    AdapterPattern
    适配器模式

---
+++ 大话设计模式/src/AdapterPattern/Adapter.java
@@ -0,0 +1,10 @@
+package AdapterPattern;
+
+public class Adapter extends Target{
+    private Adaptee adaptee = new Adaptee();
+
+    @Override
+    public void request() {
+        adaptee.specificRequest();
+    }
+}
